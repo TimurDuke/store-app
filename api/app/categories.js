c8188e1af73bdafb0136b82fa8de67da37467e0b
Timur Kim <kimtimur01@gmail.com>
2022-10-01 12:00:29 +0600

    Added category router. (Api)

---
+++ api/app/categories.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const Categories = require('../models/Category');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const categories = await Categories.find();
+
+        res.send(categories);
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+module.exports = router;
